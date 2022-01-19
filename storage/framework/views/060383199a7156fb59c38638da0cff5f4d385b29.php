41c6c29b0279f6466687054f876efa16978fd0d9
Adhitya Hari Saputra <89956974+AdhityaHari@users.noreply.github.com>
2022-01-19 08:37:59 +0700

    Add files via upload

---
+++ storage/framework/views/060383199a7156fb59c38638da0cff5f4d385b29.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Buat Account Baru!</h1>
+    <h2>Sign Up Form</h2>
+    <form action="/kirim" method="POST">
+      <label>First name</label><br /><br />
+      <input type="text" name="first name" /><br /><br />
+      <label>Last name</label><br /><br />
+      <input type="text" name="last name" /><br /><br />
+      <label>Gender :</label><br /><br />
+      <input type="radio" name="gender" />Male <br />
+      <input type="radio" name="gender" />Female <br />
+      <input type="radio" name="gender" />Other <br /><br />
+      <label>Nationality :</label><br /><br />
+      <select name="nationality">
+        <option value="1">Indonesia</option>
+        <option value="2">Malaysia</option>
+        <option value="3">Singapura</option>
+        <option value="4">Jepang</option></select
+      ><br /><br />
+      <label>Language Spoken :</label><br /><br />
+      <input type="Checkbox" />Bahasa Indonesia <br />
+      <input type="Checkbox" />English <br />
+      <input type="Checkbox" />Other <br /><br />
+      <label>Bio :</label><br /><br />
+      <textarea name="bio" cols="30" rows="10"></textarea><br /><br />
+      <input type="Submit" values="kirim" />
+    </form>
+  </body>
+</html>
+<?php /**PATH C:\Users\user\Documents\blog\resources\views/Form.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/9ca4b9ff0265e03f4c5ff1f375f647fcf888381a.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Sanberbook</h1>
+    <h2>Social Media Developer Santai Berkualitas</h2>
+    <p>Belajar dan Berbagi agar hidup ini semakin santai dan berkualitas</p>
+    <h3>Benefit Join di SanberBook</h3>
+    <ul>
+      <li>Mendapatkan motivasi dari sesama developer</li>
+      <li>Sharing knowledge dari para mastah Sanber</li>
+      <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3>Cara Bergabug ke SanberBook</h3>
+    <ol>
+      <li>Mengunjungi Website ini</li>
+      <li>Mendaftar di <a href="/daftar">Form Sign Up</a></li>
+      <li>Selesai</li>
+    </ol>
+  </body>
+</html>
+<?php /**PATH C:\Users\user\Documents\blog\resources\views/welcome.blade.php ENDPATH**/ ?>
